0250a28b0828af3a05ce0c79cde787e4fb08ca01
Britt Yazel <bwyazel@gmail.com>
2021-04-30 20:33:14 -0700

    Move defaults to their own file

--- ViragDevTool.lua
+++ ViragDevTool.lua
@@ -35,7 +35,7 @@ ViragDevTool.colors["ok"] = CreateColorFromHexString("FF00FF00")
 --- do init tasks here, like loading the Saved Variables
 --- or setting up slash commands.
 function ViragDevTool:OnInitialize()
-    self:CreateDefaultOptions()
+
 end
 
 --- **OnEnable** which gets called during the PLAYER_LOGIN event, when most of the data provided by the game is already present.
@@ -88,73 +88,6 @@ function ViragDevTool:OnDisable()
 end
 
 -------------------------------------------------
-
-function ViragDevTool:CreateDefaultOptions()
-    -- Default settings
-    -- this variable will be used only on first load so it is just default init with empty values.
-    -- will be replaced with ViragDevTool_Settings at 2-nd start
-    self.default_settings = {
-        -- selected list in gui. one of 3 list from settings: history or function call logs or events
-        sideBarTabSelected = "history",
-
-        -- UI saved state
-        isWndOpen = true,
-        isSideBarOpen = false,
-
-        -- stores history of recent calls to /vdt
-        MAX_HISTORY_SIZE = 50,
-        collResizerPosition = 450,
-        history = {
-            -- examples
-            "find LFR",
-            "find SLASH",
-            "find Data ViragDevTool",
-            "startswith Virag",
-            "ViragDevTool.settings.history",
-        },
-        logs = {--{
-            --    fnName = "functionNameHere",
-            --    parentTableName = "ViragDevTool.sometable",
-            --    active = false
-            --},
-        },
-
-        -- stores arguments for function calls --todo implement
-        tArgs = {},
-        fontSize = 10, -- font size for default table
-        colorVals = {
-            ["table"] = {0.41,0.80,0.94,1},
-            ["string"] = {0.67,0.83,0.45,1},
-            ["number"] = {1,0.96,0.41,1},
-            ["function"] =  {1,0.49,0.04,1},
-            ["default"] = {1,1,1,1},
-        },
-
-        -- events to monitor
-        -- format ({event = "EVENT_NAME", unit = "player", active = true}, ...)
-        -- default events inactive
-        events = {
-            {
-                event = "ALL",
-                active = false
-            },
-            {
-                event = "CURSOR_UPDATE",
-                active = false
-            },
-            {
-                event = "UNIT_AURA",
-                unit = "player",
-                active = false
-            },
-            {
-                event = "CHAT_MSG_CHANNEL",
-                active = false
-            }
-        },
-    }
-end
-
 
 function ViragDevTool:CreateChatCommands()
     -- you can use /vdt find somestr parentname(can be in format _G.Frame.Button)
